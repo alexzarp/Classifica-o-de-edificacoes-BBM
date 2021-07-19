b763731e4d17cd41281909ea795de2e9e82adc72
alexzarp <alexszds@gmail.com>
2021-07-19 20:59:26 -0300

    Base do projeto

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="pt-br" >
+<head>
+    <title>Classificação de Edificações</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Alex Sandro Zarpelon, Bruna Gabriela Disner, Rafael Gama Fernandes"> <!--Rafael por favor conferir o nome-->
+    <meta name="description" content="Este sistema tem como objetivo agilizar a classificação e liberação de edificações.">
+    <meta name="keywords" content="Bombeiros SC, Edificações, Alavará, Fogo, Segurança">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="shorcut icon" href="assets/img/logo.png">
+    <link rel="stylesheet" href="assets/CSS/geral.css">
+    <!-- <link rel="stylesheet" href="index.css"> -->
+</head>
+<body>
+    <header>
+        <img src="assets/img/logo.png" alt="Logo Bombeiros" id="logo">
+        <h1 class="titulo_header">Sistema de Classificação de Edificações</h1>
+        <img src="assets/img/menu.svg" alt="Botão Menu" id="botao_menu">
+    </header>
+
+    <main>
+        <h1 class="vermelho">Escolha a classificação de ocupação:</h1> <br>
+
+        <div class="grade">
+            <div class="bloco">
+                <h6>A - Resiencial</h6>
+                <img src="assets/img/casa.svg" alt="Casa">
+            </div>
+    
+            <div class="bloco">
+                <h6>B - Serviço de Hospedagem</h6>
+                <img src="assets/img/hotel.svg" alt="Hotel">
+            </div>
+    
+            <div class="bloco">
+                <h6>C - Comercial</h6>
+                <img src="assets/img/comercio.svg" alt="Comercio">
+            </div>
+    
+            <div class="bloco">
+                <h6>D - Serviço profissional</h6>
+                <img src="assets/img/profissional.svg" alt="Profissional">
+            </div>
+    
+            <div class="bloco">
+                <h6>E - Educacional e<br>Cultura física</h6>
+                <img src="assets/img/educacao.svg" alt="Educacão">
+            </div>
+    
+            <div class="bloco">
+                <h6>F - Local de Reunião de Público</h6>
+                <img src="assets/img/reuniao.svg" alt="Reunião">
+            </div>
+    
+            <div class="bloco">
+                <h6>G - Serviço automotivo e<br>assemelhados</h6>
+                <img src="assets/img/carro.svg" alt="Carro">
+            </div>
+    
+            <div class="bloco">
+                <h6>H - Serviço de saúde e<br>institucional</h6>
+                <img src="assets/img/hospital.svg" alt="Hospital">
+            </div>
+    
+            <div class="bloco">
+                <h6>I - Indústria</h6>
+                <img src="assets/img/industria.svg" alt="Indústria">
+            </div>
+    
+            <div class="bloco">
+                <h6>J - Depósito</h6>
+                <img src="assets/img/deposito.svg" alt="Depósito">
+            </div class="bloco">
+    
+            <div class="bloco">
+                <h6>K - Energia</h6>
+                <img src="assets/img/energia.svg" alt="Energia">
+            </div>
+    
+            <div class="bloco">
+                <h6>L - Explosivo</h6>
+                <img src="assets/img/explosivo.svg" alt="Explosivo">
+            </div>
+    
+            <div class="bloco">
+                <h6>M - Especial</h6>
+                <img src="assets/img/especial.svg" alt="Especial">
+            </div>
+        </div>
+
+    </main>
+
+    <footer></footer>
+</body>
+</html>
